c52d215bc37194b92a0717d48d14df514c172e2c
Shreyas Shah <shreyaslshah@gmail.com>
2021-10-26 16:40:51 +0530

    shreyas error handling commit

--- routes/team.js
+++ routes/team.js
@@ -6,35 +6,54 @@ const Teams = require("../models/TeamModel");
 const ShortUniqueId = require("short-unique-id");
 
 //get request for now, will be changed to post request when data comes from req object
-router.post("/create-team", (req, res, next) => {
-  const { username, email, teamName } = req.body;
+router.post("/create-team", async (req, res, next) => {
+
+  const { username, pocEmail, teamName } = req.body;
   const uid = new ShortUniqueId({ length: 10 });
 
   try {
-    const team = new Teams({
-      _id: new mongoose.Types.ObjectId(),
+    const team = await Teams.create({
+
       teamName: teamName,
       teamID: uid(),
-      pocEmail: email,
+      pocEmail: pocEmail,
       teamMembers: [username],
     });
-
-    team.save();
-    return res.status(200).send(team);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send("Unable to create team!");
+    return res.status(200).json({ team });
   }
+  catch (err) {
+    let errorMessage = { pocEmail: '', teamName: '' };
+    if (err.code === 11000) {
+      if (err.keyValue.teamName) {
+        errorMessage.teamName = 'That team name is not available';
+      }
+      if (err.keyValue.pocEmail) {
+        errorMessage.pocEmail = 'That email is already registered';
+      }
+    }
+    if (err.message.includes('Team validation failed')) {
+      Object.values(err.errors).forEach((err) => {
+        errorMessage[err.properties.path] = err.properties.message;
+      });
+    }
+    return res.status(500).json({ errorMessage });
+  }
+
 });
 
 router.post("/join-team", async (req, res, next) => {
-  const { username, teamID } = req.body;
-  try {
 
+  const { username, teamID } = req.body;
+
+  try {
     const checkDuplicate = await Teams.find({ "teamMembers": username });
     if (checkDuplicate.length !== 0) {
-      res.status(500).send('the user has already joined a team');
-      return;
+      return res.status(500).json({ error: 'the user has already joined a team' });
+    }
+
+    const team = await Teams.find({ "teamID": teamID });
+    if (team.length === 0) {
+      return res.status(500).json({ error: 'please enter correct team ID' })
     }
 
     const updatedTeam = await Teams.findOneAndUpdate(
@@ -52,6 +71,16 @@ router.post("/leave-team", async (req, res, next) => {
   const { username, teamID } = req.body;
 
   try {
+    const team = await Teams.find({ "teamID": teamID });
+    console.log(team);
+    if (team.length === 0) {
+      return res.status(500).json({ error: 'please enter correct team ID' })
+    }
+    if (username === team[0].teamMembers[0]) {
+      res.status(500).json({ error: 'the team leader cannot leave the team' });
+      return;
+    }
+
     const updatedTeam = await Teams.findOneAndUpdate(
       teamID,
       { $pull: { teamMembers: username } }
@@ -59,7 +88,7 @@ router.post("/leave-team", async (req, res, next) => {
     res.status(200).send(updatedTeam);
   } catch (error) {
     console.log(error);
-    res.status(500).send(error);
+    res.status(500).json({ error: 'error' })
   }
 });
 
@@ -67,16 +96,19 @@ router.post("/delete-team", async (req, res, next) => {
   const { username, teamID } = req.body;
 
   try {
-    const team = await Teams.findOne({ "teamID": teamID });
-    if (username!==team.teamMembers[0]) {
-      res.status(500).send('only the team leader can delete a team');
+    const team = await Teams.find({ "teamID": teamID });
+    if (team.length === 0) {
+      return res.status(500).json({ error: 'please enter correct team ID' })
+    }
+    if (username !== team[0].teamMembers[0]) {
+      res.status(500).json({ error: 'only the team leader can delete a team' });
       return;
     }
     await Teams.deleteOne({ "teamID": teamID });
     res.status(200).send('team deleted');
   } catch (error) {
     console.log(error);
-    res.status(500).send(error);
+    res.status(500).json({ error: 'error' });
   }
 });
 
